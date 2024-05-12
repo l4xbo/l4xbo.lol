9818ae2cb4088c499dc0e59af0ca71fac2e4e07f
l4xbo <150504950+l4xbo@users.noreply.github.com>
2024-05-11 22:15:50 -0300

    Delete js directory

--- js/rar.js
+++
@@ -1,3 +0,0 @@
-//to add more titles do ,"yourtitle" etc.
-const rrr=["@restrict","skibidi.gay","@gst","rich"];let vvv=0;function yyy(){document.title=rrr[vvv];vvv=(vvv+1)%rrr.length}
-setInterval(yyy,250)
